51c7bac0501de4bddb39d3b4a8024705d4d00245
Matheus Tanaka <matheustanakanog@gmail.com>
2021-10-22 16:37:53 -0300

    feat(init): ༼ つ ◕_◕ ༽つ initial commit

---
+++ exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/Cliente.java
@@ -0,0 +1,34 @@
+package br.senac.exemplo_cadastro;
+
+public class Cliente {
+	private int id;
+	private String nome;
+	private String cpf;
+	
+	//Getters
+	public int getId() {
+		return id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public String getCpf() {
+		return cpf;
+	}
+	
+	//Setters
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public void set(String nome) {
+		this.nome = nome;
+	}
+	
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+}
