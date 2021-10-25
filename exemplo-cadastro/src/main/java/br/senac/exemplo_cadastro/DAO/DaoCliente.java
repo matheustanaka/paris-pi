2315aeeea792f03d2ae3a808ed59cf27509834c3
Matheus Tanaka <matheustanakanog@gmail.com>
2021-10-25 20:50:10 -0300

    feat(java): criando as Daos do cliente

---
+++ exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/DAO/DaoCliente.java
@@ -0,0 +1,52 @@
+package br.senac.exemplo_cadastro.DAO;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.senac.exemplo_cadastro.BancoDeDados.DB;
+import br.senac.exemplo_cadastro.Modelos.Cliente;
+
+public class DaoCliente {
+	
+	public static void inserirCliente (Cliente cliente) throws Exception{
+		
+		String sql = "INSERT INTO cliente (nome, cpf, email, senha) VALUES (?, ?, ?, ?)";
+		
+		try (PreparedStatement ps = DB.connect().prepareStatement(sql)) {
+			ps.setString(1, cliente.getNome());
+			ps.setString(2, cliente.getCpf());
+			ps.setString(3, cliente.getEmail());
+			ps.setString(4, cliente.getSenha());
+			
+			ps.execute();
+			
+		}
+	}
+	
+	public static List<Cliente> listar() throws Exception {
+		String sql = "SELECT * FROM cliente";
+		
+		List<Cliente> resultados = new ArrayList<Cliente>();
+		
+		try (PreparedStatement ps = DB.connect().prepareStatement(sql)) {
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				Cliente cliente = new Cliente ();
+				
+				cliente.setId(rs.getInt("id"));
+				cliente.setNome(rs.getString("nome"));
+				cliente.setCpf(rs.getString("cpf"));
+				cliente.setEmail(rs.getString("email"));
+				cliente.setSenha(rs.getString("senha"));
+				
+				resultados.add(cliente);
+				
+			}
+		}
+		
+		return resultados;
+	}
+}
