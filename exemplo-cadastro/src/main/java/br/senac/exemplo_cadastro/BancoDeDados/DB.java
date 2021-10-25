64d2a3948dd7c828a0c5cf24b4825c271c0482a6
Matheus Tanaka <matheustanakanog@gmail.com>
2021-10-25 20:49:39 -0300

    fix(database): arrumando o pathing do banco

--- exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/BancoDeDados/DB.java
+++ exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/BancoDeDados/DB.java
@@ -9,7 +9,7 @@ public class DB {
 	public static Connection connect() throws Exception {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		return DriverManager.getConnection(
-				"jdbc:mysql://localhost:3306/banco", 
+				"jdbc:mysql://localhost:3306/cliente", 
 				"root", 
 				"root123");
 	}
