b4b2fd48fde5b5a7bc4b755658f8ee7f597f8969
Matheus Tanaka <matheustanakanog@gmail.com>
2021-10-25 21:34:41 -0300

    feat(java): Adicionando DAO de pesquisar, atualizar e deletar

--- exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/DAO/DaoCliente.java
+++ exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/DAO/DaoCliente.java
@@ -49,4 +49,59 @@ public class DaoCliente {
 		
 		return resultados;
 	}
+	
+	public static void excluir(int id) throws Exception{
+			
+			String sql = "DELETE FROM cliente WHERE id = ?";
+			
+			try (PreparedStatement ps = DB.connect().prepareStatement(sql)) {
+				ps.setInt(1, id);
+				ps.execute();
+				
+			}
+		}
+	
+	public static void atualizar(Cliente cliente) throws Exception{
+		
+		String sql = "UPDATE cliente SET nome = ?, cpf = ?, email = ?, senha = ? WHERE id = ?";
+		
+		try (PreparedStatement ps = DB.connect().prepareStatement(sql)) {
+			
+			ps.setString(1, cliente.getNome());
+			ps.setString(2, cliente.getCpf());
+			ps.setString(3, cliente.getEmail());
+			ps.setString(4, cliente.getSenha());
+			ps.setInt(5, cliente.getId());
+			
+			ps.execute();
+		}
+	}
+	
+	public static List<Cliente> pesquisar(String nome) throws Exception{
+		
+		String sql = "SELECT * FROM cliente WHERE nome LIKE ?";
+		
+		List<Cliente> resultados = new ArrayList<Cliente>();
+
+		
+		try (PreparedStatement ps = DB.connect().prepareStatement(sql)) {
+			ps.setString(1, "%" + nome + "%");
+			
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				Cliente cliente = new Cliente ();
+				
+				cliente.setId(rs.getInt("id"));
+				cliente.setNome(rs.getString("nome"));
+				cliente.setCpf(rs.getString("cpf"));
+				cliente.setEmail(rs.getString("email"));
+				cliente.setSenha(rs.getString("senha"));
+				
+				resultados.add(cliente);
+				
+			}
+		}
+		return resultados;
+	}
 }
