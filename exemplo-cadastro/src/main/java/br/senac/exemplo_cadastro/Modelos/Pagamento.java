30303e69490b5d9550803b0109f8ad310abfa448
matheustanaka <matheustanakanog@gmail.com>
2021-11-19 18:00:23 -0300

    feat: add formas de pagamento

---
+++ exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/Modelos/Pagamento.java
@@ -0,0 +1,19 @@
+package br.senac.exemplo_cadastro.Modelos;
+
+public class Pagamento {
+	private String tipoPagamento;
+	private int id;
+	
+	public String getTipoPagamento() {
+		return tipoPagamento;
+	}
+	public void setTipoPagamento(String tipoPagamento) {
+		this.tipoPagamento = tipoPagamento;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+}
