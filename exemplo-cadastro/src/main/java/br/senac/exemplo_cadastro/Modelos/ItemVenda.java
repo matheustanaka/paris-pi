f33d9766bcc3c48a937ea0682bbe764109c2c062
matheustanaka <matheustanakanog@gmail.com>
2021-11-19 18:00:05 -0300

    feat: add item de venda

---
+++ exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/Modelos/ItemVenda.java
@@ -0,0 +1,19 @@
+package br.senac.exemplo_cadastro.Modelos;
+
+public class ItemVenda {
+	private Roupa roupa;
+	private int quantidade;
+	public Roupa getRoupa() {
+		return roupa;
+	}
+	public void setRoupa(Roupa roupa) {
+		this.roupa = roupa;
+	}
+	public int getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+}
