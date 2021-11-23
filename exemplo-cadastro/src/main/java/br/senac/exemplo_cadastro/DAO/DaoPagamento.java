0197984d9ee2393e6866c2c8f39097d100e816e6
matheustanaka <matheustanakanog@gmail.com>
2021-11-22 21:54:06 -0300

    feat: add Dao de Pagamento

---
+++ exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/DAO/DaoPagamento.java
@@ -0,0 +1,95 @@
+package br.senac.exemplo_cadastro.DAO;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import br.senac.exemplo_cadastro.BancoDeDados.DB;
+import br.senac.exemplo_cadastro.Modelos.Cliente;
+import br.senac.exemplo_cadastro.Modelos.Pagamento;
+
+public class DaoPagamento {
+	public static void inserirPagamento (Pagamento pagamento) throws Exception{
+		
+		String sql = "INSERT INTO pagamento (pagamento) VALUES (?)";
+		
+		try (PreparedStatement ps = DB.connect().prepareStatement(sql)) {
+			ps.setString(1, pagamento.getTipoPagamento());
+			
+			ps.execute();
+			
+		}
+	}
+	
+	public static List<Pagamento> listarPagamento() throws Exception {
+		String sql = "SELECT * FROM pagamento";
+		
+		List<Pagamento> resultados = new ArrayList<Pagamento>();
+		
+		try (PreparedStatement ps = DB.connect().prepareStatement(sql)) {
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				Pagamento pagamento = new Pagamento ();
+				
+				pagamento.setId(rs.getInt("id"));
+				pagamento.setTipoPagamento(rs.getString("pagamento"));
+				
+				resultados.add(pagamento);
+				
+			}
+		}
+		
+		return resultados;
+	}
+	
+	public static void excluirPagamento(int id) throws Exception{
+			
+			String sql = "DELETE FROM pagamento WHERE id = ?";
+			
+			try (PreparedStatement ps = DB.connect().prepareStatement(sql)) {
+				ps.setInt(1, id);
+				ps.execute();
+				
+			}
+		}
+	
+	public static void atualizarPagamento(Pagamento pagamento) throws Exception{
+		
+		String sql = "UPDATE cliente SET pagamento = ?";
+		
+		try (PreparedStatement ps = DB.connect().prepareStatement(sql)) {
+			
+			ps.setString(1, pagamento.getTipoPagamento());
+			
+			ps.execute();
+		}
+	}
+	
+	public static List<Pagamento> pesquisarPagamento(String pagamento) throws Exception{
+		
+		String sql = "SELECT * FROM cliente WHERE nome LIKE ?";
+		
+		List<Pagamento> resultados = new ArrayList<Pagamento>();
+
+		
+		try (PreparedStatement ps = DB.connect().prepareStatement(sql)) {
+			ps.setString(1, "%" + pagamento + "%");
+			
+			ResultSet rs = ps.executeQuery();
+			
+			while(rs.next()) {
+				Pagamento pagamento_list = new Pagamento ();
+				
+				pagamento_list.setId(rs.getInt("id"));
+				pagamento_list.setTipoPagamento(rs.getString("pagamento"));
+
+				
+				resultados.add(pagamento_list);
+				
+			}
+		}
+		return resultados;
+	}
+}
