b092f9ad0e743c535fc9a4b3becfda3234a89fa4
matheustanaka <matheustanakanog@gmail.com>
2021-11-26 19:57:09 -0300

    feat: add relatorio de vendas

--- exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/DAO/DaoVenda.java
+++ exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/DAO/DaoVenda.java
@@ -3,10 +3,14 @@ package br.senac.exemplo_cadastro.DAO;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import br.senac.exemplo_cadastro.BancoDeDados.DB;
+import br.senac.exemplo_cadastro.Modelos.Cliente;
 import br.senac.exemplo_cadastro.Modelos.ItemVenda;
+import br.senac.exemplo_cadastro.Modelos.Roupa;
 import br.senac.exemplo_cadastro.Modelos.Venda;
 
 public class DaoVenda {
@@ -65,4 +69,54 @@ public class DaoVenda {
 		   }
 	   }
    }
+   
+   public static List<Venda> gerarRelatorio(LocalDate dataInicial, LocalDate dataFinal) throws Exception{
+	   String sql = "SELECT v.numero_venda, v.data_venda, c.nome nome_cliente, r.Tipo, r.Marca, r.Preco, vr.preco_vendido, vr.quantidade_vendida FROM venda v "
+	   		+ "INNER JOIN cliente c ON v.id_cliente = c.id_cliente "
+	   		+ "INNER JOIN venda_roupa vr ON v.id_venda = vr.id_venda "
+	   		+ "INNER JOIN roupa r ON vr.id_roupa = r.id_roupa "
+	   		+ "WHERE v.data_venda >= ? AND v.data_venda <= ?";
+	   
+	   List<Venda> resultados = new ArrayList<Venda>();
+	   
+	   try(PreparedStatement ps = DB.connect().prepareStatement(sql)) {
+		   ps.setDate(1, Date.valueOf(dataInicial));
+		   ps.setDate(2, Date.valueOf(dataFinal));
+		   
+		   ResultSet rs = ps.executeQuery();
+		   
+		   Venda venda = null;
+		   
+		   while(rs.next()) {
+			   Integer numeroVenda = rs.getInt("numero_venda");
+			   if(venda == null || venda.getNumeroVenda() != numeroVenda) {
+				   venda = new Venda();
+				   
+				   venda.setNumeroVenda(numeroVenda);
+				   venda.setDataVenda(rs.getDate("data_venda").toLocalDate());
+				   
+				   Cliente cliente = new Cliente();
+				   cliente.setNome(rs.getString("nome_cliente"));
+				   
+				   venda.setCliente(cliente);
+				   resultados.add(venda);
+			   }		   
+			   
+			   Roupa roupa = new Roupa();
+			   roupa.setTipoRoupa(rs.getString("Tipo"));
+			   roupa.setMarca(rs.getString("Marca"));
+			   roupa.setPreco(rs.getFloat("Preco"));
+			   
+			   ItemVenda itemVenda = new ItemVenda();
+			   itemVenda.setPrecoVendido(rs.getFloat("preco_vendido"));
+			   itemVenda.setQuantidadeVendida(rs.getInt("quantidade_vendida"));
+			   
+			   itemVenda.setRoupa(roupa);
+			   
+			   venda.getItensVendidos().add(itemVenda);
+			   
+	   }
+   }
+	   return resultados;
+}
 }
--- exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/Serviços/ServicoVenda.java
+++ exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/Serviços/ServicoVenda.java
@@ -1,10 +1,16 @@
 package br.senac.exemplo_cadastro.Serviços;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import br.senac.exemplo_cadastro.DAO.DaoVenda;
 import br.senac.exemplo_cadastro.Modelos.Venda;
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("venda")
@@ -18,5 +24,21 @@ public class ServicoVenda {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	
+	@GET
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<Venda> gerarRelatorio(@QueryParam("dataInicial") String dataInicial,
+			@QueryParam("dataFinal") String dataFinal) {
+		try {
+			return DaoVenda.gerarRelatorio(LocalDate.parse(dataInicial), LocalDate.parse(dataFinal));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
 	}
 }
