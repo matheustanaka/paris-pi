e13370d1ef97c07c265dac164db5f51ad1bfbfa4
Matheus Tanaka <matheustanakanog@gmail.com>
2021-11-30 19:20:18 -0300

    feat: add pagamento em ItemVenda

--- back-end/exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/Modelos/ItemVenda.java
+++ back-end/exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/Modelos/ItemVenda.java
@@ -4,6 +4,7 @@ public class ItemVenda {
 	private Roupa roupa = new Roupa();
 	private int quantidadeVendida;
 	private float precoVendido;
+	private Pagamento pagamento = new Pagamento();
 	
 	public Roupa getRoupa() {
 		return roupa;
@@ -22,6 +23,12 @@ public class ItemVenda {
 	}
 	public void setPrecoVendido(float precoVendido) {
 		this.precoVendido = precoVendido;
+	}
+	public Pagamento getPagamento() {
+		return pagamento;
+	}
+	public void setPagamento(Pagamento pagamento) {
+		this.pagamento = pagamento;
 	}
 	
 }
