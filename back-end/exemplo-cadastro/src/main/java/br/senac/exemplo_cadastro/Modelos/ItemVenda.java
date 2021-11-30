80ea880842b338102dc9d579bb7c29fc91ff584e
Matheus Tanaka <matheustanakanog@gmail.com>
2021-11-30 20:58:07 -0300

    fix: tirando objeto de pagamento e roupa em Item venda

--- back-end/exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/Modelos/ItemVenda.java
+++ back-end/exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/Modelos/ItemVenda.java
@@ -4,7 +4,6 @@ public class ItemVenda {
 	private Roupa roupa = new Roupa();
 	private int quantidadeVendida;
 	private float precoVendido;
-	private Pagamento pagamento = new Pagamento();
 	
 	public Roupa getRoupa() {
 		return roupa;
@@ -23,12 +22,6 @@ public class ItemVenda {
 	}
 	public void setPrecoVendido(float precoVendido) {
 		this.precoVendido = precoVendido;
-	}
-	public Pagamento getPagamento() {
-		return pagamento;
-	}
-	public void setPagamento(Pagamento pagamento) {
-		this.pagamento = pagamento;
 	}
 	
 }
