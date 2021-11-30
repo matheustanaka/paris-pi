3ae83bc797d93501b25f69fc2bd8502a396d335a
Matheus Tanaka <matheustanakanog@gmail.com>
2021-11-30 19:21:18 -0300

    fix: tirando Roupa e Pagamento do modelo de Venda

--- back-end/exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/Modelos/Venda.java
+++ back-end/exemplo-cadastro/src/main/java/br/senac/exemplo_cadastro/Modelos/Venda.java
@@ -10,8 +10,6 @@ public class Venda {
 	private LocalDate dataVenda;
 	private List<ItemVenda> itensVendidos = new ArrayList<ItemVenda>();
 	private Cliente cliente = new Cliente ();
-	private Pagamento pagamento = new Pagamento();
-	private Roupa roupa = new Roupa();
 	
 	public List<ItemVenda> getItensVendidos() {
 		return itensVendidos;
@@ -38,23 +36,11 @@ public class Venda {
 	public void setCliente(Cliente cliente) {
 		this.cliente = cliente;
 	}
-	public Pagamento getPagamento() {
-		return pagamento;
-	}
-	public void setPagamento(Pagamento pagamento) {
-		this.pagamento = pagamento;
-	}
 	public Integer getId_venda() {
 		return id_venda;
 	}
 	public void setId_venda(Integer id_venda) {
 		this.id_venda = id_venda;
-	}
-	public Roupa getRoupa() {
-		return roupa;
-	}
-	public void setRoupa(Roupa roupa) {
-		this.roupa = roupa;
 	}
 	
 }
